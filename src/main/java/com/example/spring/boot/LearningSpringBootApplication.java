804325a569731bff200ab7ac9b7d41ae6916b47c
puroc <471049931@qq.com>
2016-10-23 11:54:35 +0800

    Initial commit

---
+++ src/main/java/com/example/spring/boot/LearningSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+}
