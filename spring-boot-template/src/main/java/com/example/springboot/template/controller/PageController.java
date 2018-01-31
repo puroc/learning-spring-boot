e553e04e7f14a1fc9ede4e51e00c1060d7b55d18
John Doe <johndoe@doebrothers.com>
2018-01-31 18:46:55 +0800

    1

--- spring-boot-template/src/main/java/com/example/springboot/template/controller/PageController.java
+++ spring-boot-template/src/main/java/com/example/springboot/template/controller/PageController.java
@@ -17,10 +17,38 @@ public class PageController {
         return "frame/frame";
     }
 
-    @RequestMapping("/text")
+    @RequestMapping("/table")
     public ModelAndView text(ModelMap model) {
-        model.addAttribute("name", "张三");
-        return new ModelAndView("text", model);
+        List<User> userList = new ArrayList<User>();
+        userList.add(new User() {{
+            this.setName("张三");
+            this.setAge(20);
+        }});
+        userList.add(new User() {{
+            this.setName("李四");
+            this.setAge(25);
+        }});
+        userList.add(new User() {{
+            this.setName("王五");
+            this.setAge(30);
+        }});
+        model.addAttribute("userList", userList);
+
+        HashMap<String, User> userMap = new HashMap<String, User>();
+        userMap.put("1", new User() {{
+            this.setName("张三");
+            this.setAge(20);
+        }});
+        userMap.put("2", new User() {{
+            this.setName("李四");
+            this.setAge(25);
+        }});
+        userMap.put("3", new User() {{
+            this.setName("王五");
+            this.setAge(30);
+        }});
+        model.addAttribute("userMap", userMap);
+        return new ModelAndView("table", model);
     }
 
     @RequestMapping("/variable")
--- spring-boot-template/src/main/java/com/example/springboot/template/domain/User.java
+++ spring-boot-template/src/main/java/com/example/springboot/template/domain/User.java
@@ -3,6 +3,15 @@ package com.example.springboot.template.domain;
 public class User {
 
     private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public void setName(String name) {
         this.name = name;
